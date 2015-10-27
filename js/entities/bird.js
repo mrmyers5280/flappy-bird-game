a66b6fedb659b9d2411f98cd8bd37ee8665e4f06
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-10-27 11:45:42 -0600

    With entities/bird.js, add graphics component

--- js/entities/bird.js
+++ js/entities/bird.js
@@ -1,8 +1,11 @@
+var graphicsComponent = require("../components/graphics/bird");
+
 var Bird = function() {
 	console.log("Creating Bird Entity");
 
+	var graphics = new graphicsComponent.BirdGraphicsComponent(this);
 	this.components = {
-
+		graphics: graphics
 	};
 };
 
