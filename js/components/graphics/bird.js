86c1e525d576578d0403fca0300246f403fa0920
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-05 11:43:47 -0700

    With graphics/bird.js, add code to draw a circle for testing purposes

--- js/components/graphics/bird.js
+++ js/components/graphics/bird.js
@@ -2,8 +2,13 @@ var BirdGraphicsComponent = function(entity) {
 	this.entity = entity;
 };
 
-BirdGraphicsComponent.prototype.draw = function() {
+BirdGraphicsComponent.prototype.draw = function(context) {
 	console.log("Drawing a bird");
+	// Draw a circle for testing purposes
+	context.beginPath();
+	context.arc(0, 0.1, 0.1, 0, 2 * Math.PI);
+	context.fillStyle = "red";
+	context.fill();
 };
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;
