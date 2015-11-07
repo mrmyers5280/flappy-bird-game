e7be0550852773788848370b768b14823380494f
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-07 11:01:43 -0700

    With flappy_bird.js, add another instance of pipe.Pipe()

--- js/flappy_bird.js
+++ js/flappy_bird.js
@@ -5,7 +5,7 @@ var bird = require('./entities/bird');
 var pipe = require('./entities/pipe');
 
 var FlappyBird = function() {
-	this.entities = [new bird.Bird(), new pipe.Pipe()];
+	this.entities = [new bird.Bird(), new pipe.Pipe(), new pipe.Pipe()];
 	this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
 	this.physics = new physicsSystem.PhysicsSystem(this.entities);
 	this.input = new inputSystem.InputSystem(this.entities);
