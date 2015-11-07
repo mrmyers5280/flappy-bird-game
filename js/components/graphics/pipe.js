ebe1bca4bc02b112636371a76e8c6bda29f5a012
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-07 10:14:44 -0700

    With graphics/pipe.js, add code to draw a pipe

--- js/components/graphics/pipe.js
+++ js/components/graphics/pipe.js
@@ -2,8 +2,16 @@ var PipeGraphicsComponent = function(entity) {
 	this.entity = entity;
 };
 
-PipeGraphicsComponent.prototype.draw = function() {
-	console.log("Drawing a pipe");
+PipeGraphicsComponent.prototype.draw = function(context) {
+	var position = { x: 0.75, y: 0 };
+
+	context.save();
+	context.translate(position.x, position.y);
+	context.beginPath();
+	context.fillStyle = "green";
+	context.fillRect(0, 0, 0.1, 0.2);
+	context.closePath();
+	context.restore();
 };
 
 exports.PipeGraphicsComponent = PipeGraphicsComponent;
