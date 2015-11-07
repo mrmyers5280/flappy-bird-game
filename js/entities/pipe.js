bed380884ca10e5264131a6006251d0926a70a11
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-07 10:23:18 -0700

    With entities/pipe.js, add physics to the pipe entity

--- js/entities/pipe.js
+++ js/entities/pipe.js
@@ -1,10 +1,14 @@
 var graphicsComponent = require("../components/graphics/pipe");
+var physicsComponent = require("../components/physics/physics");
 
 var Pipe = function() {
-	console.log("Creating Pipe entity");
+	// console.log("Creating Pipe entity");
+	var physics = new physicsComponent.PhysicsComponent(this);
 
 	var graphics = new graphicsComponent.PipeGraphicsComponent(this);
+
 	this.components = {
+		physics: physics,
 		graphics: graphics
 	};
 };
