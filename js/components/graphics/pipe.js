b6db870dbb399c94ceaa56b85c533b766bbea0c9
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-07 11:02:54 -0700

    With graphics/pipe.js, draw second, top-hanging pipe

--- js/components/graphics/pipe.js
+++ js/components/graphics/pipe.js
@@ -3,13 +3,25 @@ var PipeGraphicsComponent = function(entity) {
 };
 
 PipeGraphicsComponent.prototype.draw = function(context) {
-	var position = { x: 0.75, y: 0 };
+	// var position = this.entity.components.physics.position;
+	var position = { x: 1, y: 0 };
 
 	context.save();
 	context.translate(position.x, position.y);
 	context.beginPath();
 	context.fillStyle = "green";
 	context.fillRect(0, 0, 0.1, 0.2);
+	context.closePath();
+	context.restore();
+
+	// top/hanging pipe
+	position = { x: 1, y: 0.4 };
+
+	context.save();
+	context.translate(position.x, position.y);
+	context.beginPath();
+	context.fillStyle = "green";
+	context.fillRect(0, 0, 0.1, 0.6);
 	context.closePath();
 	context.restore();
 };
