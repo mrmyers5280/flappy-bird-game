bc09a4894f0576b60cc9fae98116b82c745236d0
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-08 11:45:30 -0700

    With entities/pipe.js, add physics to make pipe move across screen

--- js/entities/pipe.js
+++ js/entities/pipe.js
@@ -1,9 +1,12 @@
 var graphicsComponent = require("../components/graphics/pipe");
 var physicsComponent = require("../components/physics/physics");
 
-var Pipe = function() {
+var Pipe = function(position) {
 	// console.log("Creating Pipe entity");
 	var physics = new physicsComponent.PhysicsComponent(this);
+	physics.position.x = position.x;
+	physics.position.y = position.y;
+	physics.velocity.x = -0.1;	// move the pipes towards the bird
 
 	var graphics = new graphicsComponent.PipeGraphicsComponent(this);
 
