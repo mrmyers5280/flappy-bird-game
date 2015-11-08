b2bc2940467da2d0539f0a4459aa8a7495125402
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-08 11:46:10 -0700

    With flappy_bird.js, add pipesSystem

--- js/flappy_bird.js
+++ js/flappy_bird.js
@@ -1,20 +1,23 @@
 var graphicsSystem = require('./systems/graphics');
 var physicsSystem = require('./systems/physics');
 var inputSystem = require('./systems/input');
+var pipesSystem = require('./systems/pipes');
 var bird = require('./entities/bird');
 var pipe = require('./entities/pipe');
 
 var FlappyBird = function() {
-	this.entities = [new bird.Bird(), new pipe.Pipe(), new pipe.Pipe()];
+	this.entities = [new bird.Bird(), new pipe.Pipe({ x: 1, y: 0 }), new pipe.Pipe({ x: 1, y: 0.6 })];
 	this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
 	this.physics = new physicsSystem.PhysicsSystem(this.entities);
 	this.input = new inputSystem.InputSystem(this.entities);
+	this.pipes = new pipesSystem.PipesSystem(this.entities);
 };
 
 FlappyBird.prototype.run = function() {
 	this.graphics.run();
 	this.physics.run();
 	this.input.run();
+	this.pipes.run();
 };
 
 exports.FlappyBird = FlappyBird;
