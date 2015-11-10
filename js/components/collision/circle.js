445969b0f70c649dbf8e13abfe02d5875e7e088f
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-10 10:03:15 -0700

    With collision/circle.js, add code to collideCircle function

--- js/components/collision/circle.js
+++ js/components/collision/circle.js
@@ -15,7 +15,19 @@ CircleCollisionComponent.prototype.collidesWith = function(entity) {
 };
 
 CircleCollisionComponent.prototype.collideCircle = function(entity) {
-	return false;
+	var positionA = this.entity.components.physics.position;
+	var positionB = entity.components.physics.position; // What entity is this?
+
+	var radiusA = this.radius;
+	var radiusB = entity.components.collision.radius;
+
+	var diff = {x: positionA.x - positionB.x,
+				y: positionA.y - positionB.y};
+
+	var distanceSquared = diff.x * diff.x + diff.y * diff.y;
+	var radiusSum = radiusA + radiusB;
+
+	return distanceSquared < radiusSum * radiusSum;
 };
 
 CircleCollisionComponent.prototype.collideRect = function(entity) {
