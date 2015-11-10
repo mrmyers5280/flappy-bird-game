6e926b301708ed6edbf1a1b6221ddd1b17f4b939
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-10 11:38:19 -0700

    With entities/pipe.js, create the collision component and add a callback to notify when collision occurs

--- js/entities/pipe.js
+++ js/entities/pipe.js
@@ -1,5 +1,7 @@
 var graphicsComponent = require("../components/graphics/pipe");
 var physicsComponent = require("../components/physics/physics");
+var collisionComponent = require("../components/collision/rect");
+var settings = require("../settings");
 
 var Pipe = function(position) {
 	// console.log("Creating Pipe entity");
@@ -9,11 +11,18 @@ var Pipe = function(position) {
 	physics.velocity.x = -0.1;	// move the pipes towards the bird
 
 	var graphics = new graphicsComponent.PipeGraphicsComponent(this);
+	var collision = new collisionComponent.RectCollisionComponent(this, {x: 0.1, y: 0.4});
+	collision.onCollision = this.onCollision.bind(this);
 
 	this.components = {
 		physics: physics,
-		graphics: graphics
+		graphics: graphics,
+		collision: collision
 	};
+};
+
+Pipe.prototype.onCollision = function(entity) {
+	console.log("Pipe collided with entity: ", entity);
 };
 
 exports.Pipe = Pipe;
