1f5188003fef5a12544da9f14a0fcd476681841b
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-10 11:27:16 -0700

    With entities/bird.js, create the collision component and add a callback to notify when collision occurs

--- js/entities/bird.js
+++ js/entities/bird.js
@@ -1,5 +1,7 @@
 var graphicsComponent = require("../components/graphics/bird");
 var physicsComponent = require("../components/physics/physics");
+var collisionComponent = require("../components/collision/circle");
+var settings = require("../settings");
 
 var Bird = function() {
 	// console.log("Creating Bird Entity");
@@ -8,11 +10,18 @@ var Bird = function() {
 	physics.acceleration.y = -2;	// How strong gravity is
 
 	var graphics = new graphicsComponent.BirdGraphicsComponent(this);
+	var collision = new collisionComponent.CircleCollisionComponent(this, 0.02);
+	collision.onCollision = this.onCollision.bind(this);
 
 	this.components = {
 		physics: physics,
-		graphics: graphics
+		graphics: graphics,
+		collision: collision
 	};
+};
+
+Bird.prototype.onCollision = function(entity) {
+	console.log("Bird collided with entity: ", entity);
 };
 
 exports.Bird = Bird;
