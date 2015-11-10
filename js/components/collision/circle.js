1dda0319e02512c0ea3cda0551751b8c578c2464
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-10 10:33:27 -0700

    With collision/circle.js, add code to collideRect function

--- js/components/collision/circle.js
+++ js/components/collision/circle.js
@@ -31,7 +31,34 @@ CircleCollisionComponent.prototype.collideCircle = function(entity) {
 };
 
 CircleCollisionComponent.prototype.collideRect = function(entity) {
-	return false;
+	var clamp = function(value, low, high) {
+		if (value < low) {
+			return low;
+		}
+		if (value > high) {
+			return high;
+		}
+		return value;
+	};
+
+	var positionA = this.entity.components.physics.position;
+	var positionB = entity.components.physics.position;
+	var sizeB = entity.components.collision.size;
+
+	var closest = {
+		x: clamp(positionA.x, positionB.x - sizeB.x / 2,
+				positionB.x + sizeB.x / 2),
+		y: clamp(positionA.y, positionB.y - sizeB.y / 2,
+				positionB.y + sizeB.y / 2)
+	};
+
+	var radiusA = this.radius;
+
+	var diff = {x: positionA.x - closest.x,
+				y: positionA.y - closest.y};
+
+	var distanceSquared = diff.x * diff.x + diff.y * diff.y;
+	return distanceSquared < radiusA * radiusA;
 };
 
 exports.CircleCollisionComponent = CircleCollisionComponent;
