01e9038c8ce32eac8d857724d5fc4275d7dc40bb
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-11-22 11:03:20 -0700

    With flappy_bird.js, add ground and edge entities to entity array

--- js/flappy_bird.js
+++ js/flappy_bird.js
@@ -4,9 +4,11 @@ var inputSystem = require('./systems/input');
 var pipesSystem = require('./systems/pipes');
 var bird = require('./entities/bird');
 var pipe = require('./entities/pipe');
+var ground = require('./entities/ground');
+var edge = require('./entities/edge');
 
 var FlappyBird = function() {
-	this.entities = [new bird.Bird(), new pipe.Pipe({ x: 1, y: 0 }), new pipe.Pipe({ x: 1, y: 0.6 })];
+	this.entities = [new bird.Bird(), new pipe.Pipe({ x: 1, y: 0 }), new pipe.Pipe({ x: 1, y: 0.6 }), new ground.Ground(), new edge.Edge()];
 	this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
 	this.physics = new physicsSystem.PhysicsSystem(this.entities);
 	this.input = new inputSystem.InputSystem(this.entities);
